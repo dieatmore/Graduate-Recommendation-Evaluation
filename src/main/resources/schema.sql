57ad19744a8564bf64bd53ee06227c360cc8da6b
dieatmore <s.d.wang.1@nefu.edu.cn>
2025-09-21 21:01:59 +0800

    update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,14 @@
 # 用户表
 create table if not exists `user`
 (
-    id          char(19)      primary key ,
+    id          bigint        primary key ,
     account     varchar(10)   not null ,
     password    varchar(65)   not null ,
     name        varchar(10)   not null ,
     phone       varchar(11)   null ,
     role        char(4)       not null ,
-    college_id  char(19)      not null ,
-    category_id char(19)      not null ,
-    major_id    char(19)      not null,
+    college_id  bigint        not null ,
+    major_id    bigint        not null,
 
     create_time datetime         not null default current_timestamp,
     update_time datetime         not null default current_timestamp on update current_timestamp,
@@ -21,7 +20,7 @@ create table if not exists `user`
 # 学院表
 create table if not exists `college`
 (
-    id         char(19)      primary key ,
+    id         bigint        primary key ,
     name       varchar(10)   not null ,
 
     create_time datetime         not null default current_timestamp,
@@ -31,25 +30,23 @@ create table if not exists `college`
 # 类别表
 create table if not exists `category`
 (
-    id         char(19)      primary key ,
+    id         bigint        primary key ,
     name       varchar(10)   not null ,
-    college_id char(19)      not null ,
-    user_id    char(19)      null comment '导师管理' ,
-    weight     json          not null comment '{"score", "weight"}' ,
+    college_id bigint        not null ,
+    weight     json          not null comment '{"scoreWeight", "scoreAll"}(加权成绩，综合成绩)' ,
 
     create_time datetime         not null default current_timestamp,
     update_time datetime         not null default current_timestamp on update current_timestamp,
 
-    index (college_id),
-    index (user_id)
+    index (college_id)
 );
 
 # 专业表
 create table if not exists `major`
 (
-    id          char(19)      primary key ,
+    id          bigint        primary key ,
     name        varchar(10)   not null ,
-    category_id char(19)      not null ,
+    category_id bigint        not null ,
 
     create_time datetime         not null default current_timestamp,
     update_time datetime         not null default current_timestamp on update current_timestamp,
@@ -57,13 +54,27 @@ create table if not exists `major`
     index (category_id)
 );
 
+# 用户与类别关系表(学院和导师一对多个类别)
+create table if not exists `user_category` 
+(
+    id           bigint     primary key ,
+    user_id      bigint     not null ,
+    category_id  bigint     not null ,
+
+    create_time datetime         not null default current_timestamp,
+    update_time datetime         not null default current_timestamp on update current_timestamp,
+    
+    index (user_id),
+    index (category_id)
+);
+
 # 成绩表
 create table if not exists `score`
 (
-    id         char(19)       primary key ,
-    user_id    char(19)       not null ,
-    scorex     double         not null check ( scorex > 0 and scorex <= 100 ) ,
-    ranking    smallint       not null ,
+    id         bigint         primary key ,
+    user_id    bigint         not null ,
+    scorex     double unsigned not null check ( scorex > 0 and scorex <= 100 ) ,
+    ranking    smallint unsigned not null ,
     status     tinyint        not null check (status in (0,1)) default 0 comment '0未认定 1已认定',
 
     create_time datetime         not null default current_timestamp,
@@ -75,14 +86,13 @@ create table if not exists `score`
 # 指标节点表
 create table if not exists `target_node`
 (
-    id            char(19)        primary key ,
-    parent_id     char(19)        null ,
+    id            bigint          primary key ,
+    parent_id     bigint          null ,
     name          varchar(100)    not null ,
-    category_id   char(19)        not null ,
-    max_mark      double          not null comment '上限分数',
-    max_number    tinyint         null comment '最大项数' ,
-    last_time     datetime        not null comment '截至日期',
-    note          text            null comment '规则说明',
+    category_id   bigint          not null ,
+    max_mark      double unsigned not null comment '上限分数',
+    max_number    tinyint unsigned null comment '最大项数' ,
+    comment          text            null comment '规则说明',
 
     create_time datetime         not null default current_timestamp,
     update_time datetime         not null default current_timestamp on update current_timestamp,
@@ -93,10 +103,10 @@ create table if not exists `target_node`
 # 指标提交表
 create table if not exists `target_submit`
 (
-    id             char(19)               primary key ,
-    user_id        char(19)               not null comment '学生id',
-    target_node_id char(19)               not null ,
-    mark           double                 null ,
+    id             bigint                 primary key ,
+    user_id        bigint                 not null comment '学生id',
+    target_node_id bigint                 not null ,
+    mark           double unsigned        null ,
     name           varchar(200)           not null comment '提交项名称' ,
     comment        text                   null comment '提交说明',
     status         tinyint                not null default 0 comment '0审核中、1待修改、2被驳回、3已认定' ,
@@ -107,5 +117,5 @@ create table if not exists `target_submit`
     update_time datetime         not null default current_timestamp on update current_timestamp,
 
     index (user_id, target_node_id, status),
-    index ((cast(record ->> '$.userId' as char(19)) collate utf8mb4_bin), status)
+    index ((cast(record ->> '$.userId' as unsigned )), status)
 );
